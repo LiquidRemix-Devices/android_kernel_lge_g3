491eb6512d5bb0597f733fe9ac91be4f82c9d775
Mao Jinlong <c_jmao@codeaurora.org>
2014-04-11 15:42:26 +0800

    alarm : Fix the race conditions in alarm-dev.c
    There will be race conditions between alarm set and alarm clear if
    set_power_on_alarm is out of the lock.But set_power_on_alarm can't
    be put into spin_lock. So add it into mutex_lock.
    
    CRs-Fixed: 639115
    Change-Id: I4226a95e499211c0d50ff7ce269467a57a410dc7
    Signed-off-by: Mao Jinlong <c_jmao@codeaurora.org>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/staging/android/alarm-dev.c
+++ drivers/staging/android/alarm-dev.c
@@ -48,6 +48,8 @@ module_param_named(debug_mask, debug_mask, int, S_IRUGO | S_IWUSR | S_IWGRP);
 static int alarm_opened;
 static DEFINE_SPINLOCK(alarm_slock);
 static struct wake_lock alarm_wake_lock;
+static DEFINE_MUTEX(alarm_mutex);
+static struct wakeup_source alarm_wake_lock;
 static DECLARE_WAIT_QUEUE_HEAD(alarm_wait_queue);
 static uint32_t alarm_pending;
 static uint32_t alarm_enabled;
@@ -106,6 +108,7 @@ static void alarm_clear(enum android_alarm_type alarm_type, struct timespec *ts)
 	uint32_t alarm_type_mask = 1U << alarm_type;
 	unsigned long flags;
 
+	mutex_lock(&alarm_mutex);
 	spin_lock_irqsave(&alarm_slock, flags);
 	alarm_dbg(IO, "alarm %d clear\n", alarm_type);
 	devalarm_try_to_cancel(&alarms[alarm_type]);
@@ -119,6 +122,7 @@ static void alarm_clear(enum android_alarm_type alarm_type, struct timespec *ts)
 
 	if (alarm_type == ANDROID_ALARM_RTC_POWEROFF_WAKEUP)
 		set_power_on_alarm(ts->tv_sec, 0);
+	mutex_unlock(&alarm_mutex);
 }
 
 static void alarm_set(enum android_alarm_type alarm_type,
@@ -127,6 +131,7 @@ static void alarm_set(enum android_alarm_type alarm_type,
 	uint32_t alarm_type_mask = 1U << alarm_type;
 	unsigned long flags;
 
+	mutex_lock(&alarm_mutex);
 	spin_lock_irqsave(&alarm_slock, flags);
 	alarm_dbg(IO, "alarm %d set %ld.%09ld\n",
 			alarm_type, ts->tv_sec, ts->tv_nsec);
@@ -136,6 +141,7 @@ static void alarm_set(enum android_alarm_type alarm_type,
 
 	if (alarm_type == ANDROID_ALARM_RTC_POWEROFF_WAKEUP)
 		set_power_on_alarm(ts->tv_sec, 1);
+	mutex_unlock(&alarm_mutex);
 }
 
 static int alarm_wait(void)
