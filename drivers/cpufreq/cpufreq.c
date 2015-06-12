03cffa943444fec2176e22a112be21c3155629a8
Alex Naidis <alex.naidis@linux.com>
2015-06-12 23:52:15 +0200

    cpufreq: Fix cores not restoring saved frequencies after replugging
    * In the scenario if a CPU core gets unplugged and replugged again, I noticed that saved frequencies (min-max) are not being restored
    * That's an unwanted behaviour and was intrduced by "cpufreq: Simplify cpufreq_add_dev()" (squashed in a large commit)
    * I am really surprised no one noticed this bug...
    * So let's get the part with the restore back :)
    
    Change-Id: I954d2a0a18f69a73e8a504dcc35fa2ddfb9888d0
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1074,6 +1074,7 @@ static int cpufreq_add_dev(struct device *dev, struct subsys_interface *sif)
 		pr_debug("Restoring governor %s for cpu %d\n",
 		       policy->governor->name, cpu);
 	}
+
 	if (per_cpu(cpufreq_policy_save, cpu).min) {
 		policy->min = per_cpu(cpufreq_policy_save, cpu).min;
 		policy->user_policy.min = policy->min;
@@ -1084,6 +1085,8 @@ static int cpufreq_add_dev(struct device *dev, struct subsys_interface *sif)
 	}
 	pr_debug("Restoring CPU%d min %d and max %d\n",
 		cpu, policy->min, policy->max);
+
+
 #endif
 
 	ret = cpufreq_add_dev_interface(cpu, policy, dev);
