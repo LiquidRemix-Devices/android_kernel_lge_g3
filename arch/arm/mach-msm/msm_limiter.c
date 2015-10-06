bfc93099d674377fa4f4ed78e4554191c8206f27
Eliminater74 <eliminater74@gmail.com>
2015-10-06 18:08:53 -0400

    MSM_LIMITER: Bump V3.5 - 3.6 - 3.8, msm: limiter: Export tunables to header file create include/linux/msm_limiter.h
    Thank you: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- arch/arm/mach-msm/msm_limiter.c
+++ arch/arm/mach-msm/msm_limiter.c
@@ -4,6 +4,7 @@
  * Copyright (c) 2013-2014, Dorimanx <yuri@bynet.co.il>
  * Copyright (c) 2013-2014, Pranav Vashi <neobuddy89@gmail.com>
  * Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2015, Eliminater74 <eliminater74@gmail.com> 
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -22,16 +23,10 @@
 #else
 #include <linux/lcd_notify.h>
 #endif
-
+#include <linux/msm_limiter.h>
 #define MSM_CPUFREQ_LIMIT_MAJOR		3
-#define MSM_CPUFREQ_LIMIT_MINOR		5
+#define MSM_CPUFREQ_LIMIT_MINOR		6
 
-#define MSM_LIMIT			"msm_limiter"
-#define LIMITER_ENABLED			1
-#define DEFAULT_SUSPEND_DEFER_TIME	10
-#define DEFAULT_SUSPEND_FREQUENCY	1728000
-#define DEFAULT_RESUME_FREQUENCY	2457600
-#define DEFAULT_MIN_FREQUENCY		268800
 
 static unsigned int debug = 0;
 module_param_named(debug_mask, debug, uint, 0644);
@@ -41,34 +36,6 @@ do { 				\
 	if (debug)		\
 		pr_info(msg);	\
 } while (0)
-
-static struct cpu_limit {
-	unsigned int limiter_enabled;
-	uint32_t suspend_max_freq;
-	uint32_t resume_max_freq[4];
-	uint32_t suspend_min_freq[4];
-	unsigned int suspended;
-	unsigned int suspend_defer_time;
-	struct delayed_work suspend_work;
-	struct work_struct resume_work;
-	struct mutex resume_suspend_mutex;
-	struct mutex msm_limiter_mutex[4];
-#ifndef CONFIG_POWERSUSPEND
-	struct notifier_block notif;
-#endif
-} limit = {
-	.limiter_enabled = LIMITER_ENABLED,
-	.suspend_max_freq = DEFAULT_SUSPEND_FREQUENCY,
-	.resume_max_freq[0] = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[1] = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[2] = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[3] = DEFAULT_RESUME_FREQUENCY,
-	.suspend_min_freq[0] = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[1] = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[2] = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[3] = DEFAULT_MIN_FREQUENCY,
-	.suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME,
-};
 
 static struct workqueue_struct *limiter_wq;
 
